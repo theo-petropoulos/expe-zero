93027ee47d912c0d62a84054308af342d532d1ab
anthony-ohannessian <anthony.ohannessian@laplateforme.io>
2021-11-10 09:26:29 +0100

    header and footer done

--- view/requires/footer.php
+++ view/requires/footer.php
@@ -1,38 +1,158 @@
-    </body>
-    <footer>
-        <section class="footer_left">
-            <a id="logo_footer" href="/expe-zero/"><h1>EXPEDITION ZERO</h1></a>
-            <span class="footer_left_social">
-                <a href=""><i class="fab fa-facebook-f"></i></a>
-                <a href=""><i class="fab fa-instagram"></i></a>
-                <a href=""><i class="fab fa-linkedin-in"></i></a>
-                <a href=""><i class="fab fa-tiktok"></i></a>
-                <a href=""><i class="fab fa-twitter"></i></a>
-                <a href=""><i class="fab fa-youtube"></i></a>
-            </span>
-            <span class="footer_left_mail">
-                <i class="fas fa-at"></i>
-                <a href="mailto:benjamindemolliens@gmail.com">benjamindemolliens@gmail.com</a>
-            </span>
-        </section>
-        <section class="footer_middle">
-            <span class="footer_nav_container">
-                <a href="aventures">AVENTURES</a>
-                <a href="defis">DÉFIS</a>
-                <a href="services">OBJECTIF ZERO</a>
-                <a href="about">À PROPOS</a>
-                <a href="https://en.tipeee.com/expedition-zero" id="red_item_footer">SOUTENIR EXPE ZERO</a>
-            </span>
-        </section>
-        <section class="footer_right">
-            <span>
-                <img src="/expe-zero/assets/images/partenaires/maif.png">
-                <img src="/expe-zero/assets/images/partenaires/sncf.png">
-            </span>
-            <span>
-                <img src="/expe-zero/assets/images/partenaires/flixbus.png">
-                <img src="/expe-zero/assets/images/partenaires/blablabus.png">
-            </span>
-        </section>
+    <!-- Footer Section start -->
+    <footer class="page-footer font-small white pt-4 bg-dark text-white">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+  
+      <!-- Grid row -->
+      <div class="row">
+  
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+  
+          <!-- Content -->
+          <h5 class="text-uppercase text-white">
+            <a class="nav-link active uk-link-reset" aria-current="page" href="#">Expédition Zéro</a>
+            <a class="nav-link active uk-link-reset" aria-current="page" href="#"><h6 class="text-white">Suivez-moi</h6></a>
+          </h5>
+          <!-- Grid column -->
+          <div class="col-md-12 py-5 p-1">
+            <div class="mb-5 flex-center">
+
+            <!-- Facebook -->
+            <a class="fb-ic p-4" style="color: white !important;">
+                <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            
+            <!-- Twitter -->
+            <a class="tw-ic p-4" style="color: white !important;">
+                <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            
+            <!-- Google +-->
+            <a class="gplus-ic p-4" style="color: white !important;">
+                <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            
+            <!--Linkedin -->
+            <a class="li-ic p-4" style="color: white !important;">
+                <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            
+            <!--Instagram-->
+            <a class="ins-ic p-4" style="color: white !important;">
+                <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            
+            <!--Pinterest-->
+            <a class="pin-ic p-4" style="color: white !important;">
+                <i class="fab fa-pinterest fa-lg white-text fa-2x"></i>
+            </a>
+            </div>
+
+            <!-- Content -->
+          <h5 class="text-uppercase text-white">
+            <h6 class="text-white">Ou écrivez-moi à</h6>
+            <a class="nav-link active uk-link-reset" aria-current="page" href="#"><h6 class="text-white">benjamindemolliens@gmail.com</h6></a>
+          </h5>
+        </div>
+        <!-- Grid column -->
+  
+        </div>
+        <!-- Grid column -->
+  
+        <hr class="clearfix w-100 d-md-none pb-3">
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase text-white">
+            <a class="nav-link active uk-link-reset" aria-current="page" href="#">Pages</a>
+          </h5>
+
+          <ul class="list-unstyled bg-dark text-white">
+            <li>
+                <button class="uk-button uk-button-text text-white">
+                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                        Aventures
+                    </a>
+                </button>
+            </li>
+            <li>
+                <button class="uk-button uk-button-text text-white">
+                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                        Défi
+                    </a>
+                </button>
+            </li>
+            <li>
+                <button class="uk-button uk-button-text text-white">
+                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                        Objectif Zero
+                    </a>
+                </button>
+            </li>
+            <li>
+                <button class="uk-button uk-button-text text-white">
+                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                        A propos
+                    </a>
+                </button>
+            </li>
+            <li>
+                <button class="uk-button uk-button-text text-white">
+                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                        Soutenir Expe Zero
+                    </a>
+                </button>
+            </li>
+          </ul>
+        </div>
+        <!-- Grid column -->
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase text-white">Partenaires</h5>
+          <ul class="list-unstyled text-white">
+            <li>
+                <button class="uk-button uk-button-text text-white">
+                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                        <img class="img-fluid" src="https://res.cloudinary.com/afohann/image/upload/v1636531455/Logo_Maif_2019.svg_mxroox.png" uk-responsive>
+                    </a>
+                </button>
+            </li>
+          </ul>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+    <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3 pb-4">
+            Crédits @expeditionzero, créé avec enthousiasme par code4marseille
+        </div>
+        <!-- Copyright -->
     </footer>
+    <!-- Footer Section end -->
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.6/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.6/dist/js/uikit-icons.min.js"></script>
+
+    <!-- MDBootstrap -->
+    <script type="text/javascript" src="node_modules/mdbootstrap/js/jquery.min.js"></script>
+    <script type="text/javascript" src="node_modules/mdbootstrap/js/popper.min.js"></script>
+    <script type="text/javascript" src="node_modules/mdbootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="node_modules/mdbootstrap/js/mdb.min.js"></script>
+
+</body>
 </html>
--- view/requires/header.php
+++ view/requires/header.php
@@ -1,69 +1,160 @@
-<nav id="nav_bar">
-    <span id="logo_left">
-        <h1>
-            <a href="/expe-zero/">EXPEDITION<br>ZERO</a>
-        </h1>
-    </span>
-    <span id="menu_right">
-        <ul id="menu_items">
-            <li class="menu_item">
-                <a href="aventures">AVENTURES</a>
-            </li>
-            |
-            <li class="menu_item">
-                <a href="defis">DÉFIS</a>
-            </li>
-            |
-            <li class="menu_item">
-                <a href="services">OBJECTIF ZERO</a>
-                <ul class="submenu_items">
-                    <li class="submenu_item">
-                        <a href="services#conférences">Conférences</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="services#défis">Défis salariés</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="services#ateliers">Ateliers climat</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="services#bilan">Bilan carbone</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="services#acv">ACV</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="services#conception">Éco-conception</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="services#labellisation">Labellisation</a>
-                    </li>
-                </ul>
-            </li>
-            |
-            <li class="menu_item">
-                <a href="about">À PROPOS</a>
-                <ul class="submenu_items">
-                    <li class="submenu_item">
-                        <a href="about#who">Qui suis-je ?</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="about#medias">Médias</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="about#partenaires">Partenaires</a>
-                    </li>
-                    <li class="submenu_item">
-                        <a href="contact">Contact</a>
-                    </li>
-                </ul>
-            </li>
-            |
-            <li class="menu_item"  id="red_item">
-                <a href="https://en.tipeee.com/expedition-zero">SOUTENIR EXPE ZERO</a>
-            </li>
-        </ul>
-    </span>
-</nav>
+<header class="justify-content-between pt-4" style="font-family: 'Roboto Mono', monospace;">
+        <!-- Navbar start -->
+        <!-- <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky"> -->
+            <div>
+            <nav class="navbar navbar-expand-md bg-white navbar-light uk-navbar-container">
+                <div class="container container-fluid">
 
-<body>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+                        <li class="nav-item p-4">
+                            <li class="nav-item fs-1">
+                                <a class="nav-link active uk-link-reset" aria-current="page" href="#">Expédition<div class="text-break">Zéro</div></a>
+                            </li>
+                        </li>
+                    </ul>
+
+                    <div class="collapse navbar-collapse" id="navbarScroll">
+                        <ul class="navbar-nav me-auto my-2 my-lg-0 justify-content-end" style="--bs-scroll-height: 100px;">
+                            <li class="nav-item p-3">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
+                                    <button class="uk-button uk-button-text">
+                                        <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                            Aventures
+                                        </a>
+                                    </button>
+                                </a>
+                            </li>
+                            
+                            <li class="nav-item p-3">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
+                                    <button class="uk-button uk-button-text">
+                                        <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                            Défi
+                                        </a>
+                                    </button>
+                                </a>
+                            </li>
+
+                            <!-- Dropdown start hover -->
+                            <li class="nav-item p-3">
+                                <a class="nav-link disabled" href="#" aria-disabled="true">
+                                    <button class="uk-button uk-button-text">
+                                        <a class="nav-link disabled uk-link-reset" href="" id="navbarScrollingDropdown" aria-expanded="false" aria-disabled="true" role="button" data-bs-toggle="dropdown">
+                                            Objectif Zéro
+                                        </a>
+                                    </button>
+                                    <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 500">
+                                        <ul class="uk-nav uk-dropdown-nav">
+                                            <li class="uk-active">
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Conférence
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Défis Salariés
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Ateliers Climat
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Bilan Carbone
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        ACV
+                                                    </a>
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </a>
+                            </li>
+                            <!-- Dropdown end hover -->
+
+                            <!-- Dropdown start hover -->
+                            <li class="nav-item p-3">
+                                <a class="nav-link disabled" href="#" aria-disabled="true">
+                                    <button class="uk-button uk-button-text">
+                                        <a class="nav-link disabled uk-link-reset" href="" id="navbarScrollingDropdown" aria-expanded="false" aria-disabled="true" role="button" data-bs-toggle="dropdown">
+                                            À propos
+                                        </a>
+                                    </button>
+                                    <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 500">
+                                        <ul class="uk-nav uk-dropdown-nav">
+                                            <li class="uk-active">
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Qui suis-je
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Vidéos
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Médias
+                                                    </a>
+                                                </button>
+                                            </li>
+
+                                            <li>
+                                                <button class="uk-button uk-button-text">
+                                                    <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                                        Partenaires
+                                                    </a>
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </a>
+                            </li>
+                            <!-- Dropdown end hover -->
+
+                            <li class="nav-item p-3">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
+                                    <button class="uk-button uk-button-text">
+                                        <a class="nav-link disabled uk-link-reset" href="" target="_blank" aria-disabled="true">
+                                            Soutenir Expe Zéro
+                                        </a>
+                                    </button>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <!-- Navbar end -->
+    </header>
+    <!-- Header Section end -->
